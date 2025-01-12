1d926600fa7fb3e1f5aa4902599c4c76ba034afe
agustin dell aquila <dellaquila39492276@hotmail.com>
2025-01-12 17:21:33 -0300

    componentes nuevos (cards-glasstable-calltoaction

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,29 @@
     </v-app-bar>  
 
     <v-main>  
-      <StartView v-if="$route.path === '/'"></StartView>  
-      <router-view v-else></router-view>  
+      <StartView v-if="$route.path === '/'"></StartView>
+      <v-spacer style="height: 85px" v-if="$route.path === '/'"></v-spacer>  
+      <Cards v-if="$route.path === '/'"></Cards>
+      <v-spacer style="height: 85px" v-if="$route.path === '/'"></v-spacer>  
+      <GlassTable v-if="$route.path === '/'"></GlassTable>
+      <v-spacer style="height: 85npx" v-if="$route.path === '/'"></v-spacer> 
+      <CallToAction v-if="$route.path === '/'"></CallToAction>  
+      <router-view v-else></router-view> 
     </v-main>  
   </v-app>  
 </template>  
 
 <script>  
 import NavigationBar from './components/NavigationBar.vue';    
-import StartView from './views/StartView.vue';  
+import StartView from './views/StartView.vue';
+import Cards from './components/Cards.vue';
+import GlassTable from './components/GlassTable.vue';
+import CallToAction from './components/CallToAction.vue';
 
 export default {  
   name: 'App',  
   components: {  
-    NavigationBar,StartView },   
+    NavigationBar,StartView,Cards,GlassTable,CallToAction },   
 };  
 </script>
+
---
+++ src/components/CallToAction.vue
@@ -0,0 +1,51 @@
+<template>  
+  <div class="cta-container">  
+    <div class="cta-content">  
+      <h2>¿Listo para obtener un presupuesto gratuito?</h2>  
+      <p>No dudes en contactarnos para obtener un presupuesto personalizado y resolver todas tus dudas sobre nuestros productos y servicios de vidrio.</p>  
+      <v-btn class="cta-button" @click="contactar">Contactar y obtener un presupuesto gratuito</v-btn>  
+    </div>  
+  </div>  
+</template>  
+
+<script>  
+export default {  
+  methods: {  
+    contactar() {  
+      // Aquí iría la lógica para contactar con nosotros (email, teléfono, formulario, etc.)  
+    }  
+  }  
+}  
+</script>  
+
+<style scoped>  
+.cta-container {  
+  background-image: linear-gradient(to bottom, #4567b7, #1a1d23);  
+  background-size: 100% 300px;  
+  background-position: 0% 100%;  
+  height: 300px;  
+  display: flex;  
+  justify-content: center;  
+  align-items: center;  
+}  
+
+.cta-content {  
+  max-width: 800px;  
+  margin: 0 auto;  
+  text-align: center;  
+  color: #fff;  
+}  
+
+.cta-button {  
+  background-color: #03a9f4;  
+  color: #020000;  
+  padding: 10px 20px;  
+  border: none;  
+  border-radius: 5px;  
+  cursor: pointer;  
+}  
+
+.cta-button:hover {  
+  background-color: #0377b7;  
+}  
+</style>
